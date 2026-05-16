f13b91b68c38acde4ccf32e3ae8164d1ae6c70d0
Karan5004 <karanpreet23001@gmail.com>
2026-05-16 16:49:15 +0530

    add timeout for vercel cold start

--- back-end/routes/events.js
+++ back-end/routes/events.js
@@ -2,26 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Event = require("../models/Event");
 
-
 router.get("/", async (req, res) => {
   try {
     let query = {};
-
-   
     if (req.query.type && req.query.type !== "Both") {
       query.type = req.query.type;
     }
-
-    
     if (req.query.search) {
       let searchText = req.query.search;
       query.$or = [
-        { title: { $regex: searchText, $options: "i" } }, 
+        { title: { $regex: searchText, $options: "i" } },
         { tags: { $regex: searchText, $options: "i" } },
       ];
     }
-
-    const events = await Event.find(query);
+    const events = await Event.find(query).maxTimeMS(20000);
     res.json(events);
   } catch (err) {
     console.log("error getting events:", err);
@@ -29,10 +23,9 @@ router.get("/", async (req, res) => {
   }
 });
 
-
 router.get("/:id", async (req, res) => {
   try {
-    const event = await Event.findById(req.params.id);
+    const event = await Event.findById(req.params.id).maxTimeMS(20000);
     if (!event) {
       return res.status(404).json({ message: "Event not found" });
     }
@@ -43,25 +36,21 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-
 router.post("/", async (req, res) => {
   try {
     const newEvent = new Event(req.body);
     const savedEvent = await newEvent.save();
     res.status(201).json(savedEvent);
   } catch (err) {
-    console.log("error creating event:", err);
     res.status(500).json({ message: "Something went wrong" });
   }
 });
-
 
 router.delete("/:id", async (req, res) => {
   try {
     await Event.findByIdAndDelete(req.params.id);
     res.json({ message: "Event deleted" });
   } catch (err) {
-    console.log("error deleting event:", err);
     res.status(500).json({ message: "Something went wrong" });
   }
 });
--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({ origin: "*" }));
-app.use(express.json());
+app.use(express.json()); 
 
 
 const eventRoutes = require("./routes/events");
