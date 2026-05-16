f4826264690c1619611ecf65c1d6bd82e924617a
Karan5004 <karanpreet23001@gmail.com>
2026-05-16 13:42:41 +0530

    fix changes

--- meet-up/src/App.jsx
+++ meet-up/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import EventsPage from "./pages/EventsPage";
-import EventDetailPage from "./pages/EventDetailPage";
+import Navbar from "./components/Navbar.jsx";
+import EventsPage from "./pages/EventsPage.jsx";
+import EventDetailPage from "./pages/EventDetailPage.jsx";
 
 function App() {
   return (
